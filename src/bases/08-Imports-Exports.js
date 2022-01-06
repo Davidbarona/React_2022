567dd33d51f4cfca1706f929237af40f910a4d91
unknown <davidb@soymomo.com>
2022-01-05 22:06:40 -0500

    knowlegde: Promise

--- src/bases/08-Imports-Exports.js
+++ src/bases/08-Imports-Exports.js
@@ -1,6 +1,6 @@
 import heroes, {owners}  from '../data/heroes'
 
-console.log(owners);
+// console.log(owners);
 
 // const getHeoreById = (id) => {
 //     return heroes.find((hero)=>{
@@ -15,15 +15,15 @@ console.log(owners);
 // }
 
 
-    const getHeoreById = (id) =>{
+    export const getHeoreById = (id) =>{
         return heroes.find((heroe)=> heroe.id === id)
     }
 
-console.log(getHeoreById(4));
+// console.log(getHeoreById(4));
 
-    const getHeroesByOwner = (owner) => heroes.filter((heroe)=> heroe.owner === owner)
+ export const getHeroesByOwner = (owner) => heroes.filter((heroe)=> heroe.owner === owner)
 
-console.log(getHeroesByOwner('DC'));
+// console.log(getHeroesByOwner('DC'));
 
 //When you realice an export by default you can choise the name what do you want, only you  must write 
 // the name and into the field to export, 'export default' if anoyone name  
